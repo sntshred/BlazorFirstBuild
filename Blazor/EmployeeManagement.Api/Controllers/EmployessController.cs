483179f667c798b19c2ba2908e978934a6c9498d
Santosh Chavan <santosh.aryan9@gmail.com>
2021-08-11 23:18:10 -0400

    first commit

---
+++ Blazor/EmployeeManagement.Api/Controllers/EmployessController.cs
@@ -0,0 +1,91 @@
+﻿using EmployeeManagement.Api.Models;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployessController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployessController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error Reterving the Database");
+
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            try
+            {
+
+                var result = await employeeRepository.GetEmployee(id);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                       "Error retrieving data from the database");
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+
+            try
+            {
+                if (employee == null)
+                {
+                    return BadRequest();
+                }
+
+                var emp = employeeRepository.GetEmployeeByEmail(employee.Email);
+                if (emp != null) {
+
+                    ModelState.AddModelError("email", "Employee email already in use");
+                    return BadRequest(ModelState);
+                }
+
+                var createEmployee = await employeeRepository.AddEmployee(employee);
+                return CreatedAtAction(nameof(GetEmployee), new { id = createEmployee.EmployeeId }, createEmployee);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                       "Error retrieving data from the database");
+            }
+
+
+        }
+         
+
+    }
+}
---
+++ Blazor/EmployeeManagement.Api/Models/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int employeeId);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task<Employee> GetEmployeeByEmail(string email);
+
+        void DeleteEmployee(int employeeId);
+    }
+}
